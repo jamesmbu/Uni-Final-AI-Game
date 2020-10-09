e675c1263053b8e9cc6a88cec7c45d47432194bd
James Burke <k1811535@kingston.ac.uk>
2020-10-09 22:08:55 +0100

    (extra commit)
    a file changed right after the commit on its own, not sure what happened because the file is the same, but pushing this in-case

---
+++ Source/FYP_K1811535/HealthComponent.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+#include "FYP_K1811535/Modes/FYP_K1811535GameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	ActiveHealth = MaxHealth;
+	GameModeReference = Cast<AFYP_K1811535GameModeBase>(UGameplayStatics::GetGameMode(GetWorld())); // find the currently active game mode
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage); // when the owning actor calls OnTakeAnyDamage, the dynamic delegate will be called from this class using TakeDamage
+	
+}
+
+void UHealthComponent::TakeDamage(AActor* DamagedActor, float DamageAmount, const UDamageType* DamageType, AController* EventInstigator, AActor* DamageCauser)
+{
+	if (DamageAmount == 0)
+	{
+		return;
+	}
+
+	ActiveHealth = FMath::Clamp(ActiveHealth - DamageAmount, 0.0f, MaxHealth);
+
+	if(ActiveHealth <= 0)
+	{
+		if (GameModeReference)
+		{
+			GameModeReference->DeathHandle(GetOwner());
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Health Component of %s (ID: %s) has no reference to GameMode"),*GetOwner()->GetActorLabel(), *GetOwner()->GetName());
+		}
+	}
+}
+
+
