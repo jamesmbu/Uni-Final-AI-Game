bae91625d982e63e7d2a8f1cd4cd0b070f07a3e4
James Burke <k1811535@kingston.ac.uk>
2021-03-29 20:55:57 +0100

    C++ AI Perception Component: Sight Set Up

--- Source/FYP_K1811535/MinionAIController.cpp
+++ Source/FYP_K1811535/MinionAIController.cpp
@@ -4,8 +4,17 @@
 #include "MinionAIController.h"
 #include "Kismet/GameplayStatics.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 #include "Characters/CharacterBase.h"
 
+AMinionAIController::AMinionAIController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ConfigureAIPerception();
+	
+}
 void AMinionAIController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -21,12 +30,51 @@ void AMinionAIController::BeginPlay()
 	}
 	
 }
+
+void AMinionAIController::ConfigureAIPerception()
+{
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Configuration"));
+	SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("Perception Component")));
+	SightConfig->SightRadius = AISightRadius;
+	SightConfig->LoseSightRadius = AILoseSightRadius;
+	SightConfig->PeripheralVisionAngleDegrees = AIFieldOfView;
+	SightConfig->SetMaxAge(AISightAge);
+
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+
+	AAIController::GetPerceptionComponent()->SetDominantSense(*SightConfig->GetSenseImplementation());
+	AAIController::GetPerceptionComponent()->OnPerceptionUpdated.AddDynamic(this, &AMinionAIController::OnPawnDetected);
+	GetPerceptionComponent()->ConfigureSense(*SightConfig);
+}
+
+
 void AMinionAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 	
 	
 }
+
+void AMinionAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+/*FRotator AMinionAIController::GetControlRotation() const
+{
+	if (GetPawn() == nullptr)
+	{
+		return FRotator(0.0f, 0.0f, 0.0f);
+	}
+	return FRotator(0.0f, GetPawn()->GetActorRotation().Yaw, 0.0f);
+}*/
+
+void AMinionAIController::OnPawnDetected(const TArray<AActor*>& DetectedPawns)
+{
+}
+
 
 
 
--- Source/FYP_K1811535/MinionAIController.h
+++ Source/FYP_K1811535/MinionAIController.h
@@ -10,15 +10,38 @@
  * 
  */
 class UBehaviorTree;
+class UAISenseConfig_Sight;
 UCLASS()
 class FYP_K1811535_API AMinionAIController : public AAIController
 {
 	GENERATED_BODY()
 public:
+	AMinionAIController();
 	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void OnPossess(APawn* InPawn) override; // When the character plugs into the controller
+
+	//virtual FRotator GetControlRotation() const override; // For rotation of perception
+
+	UFUNCTION()
+	void OnPawnDetected(const TArray<AActor*>& DetectedPawns); // When any pawn is detected by perception
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+	float AISightRadius = 500.0f; // Distance-based visibility
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+	float AISightAge = 5.0f; // How long an object in sight is remembered for
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+	float AILoseSightRadius = AISightRadius + 50.0f; // Distance in which AI loses sight of a previously visible object
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+	float AIFieldOfView = 90.f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+	UAISenseConfig_Sight* SightConfig;
+	
 protected:
 	virtual void BeginPlay() override;
 private:
 	UPROPERTY(EditAnywhere)
 	UBehaviorTree* AIBehaviorTree;
+
+	void ConfigureAIPerception();
 };
